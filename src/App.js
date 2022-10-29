00478b80c7d5f01968425108f7c2e2d8cdb99e34
Oghenesuvwe Money <rhounamoney@gmail.com>
2022-10-29 16:56:04 +0100

    Section 3 update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from './components/header/Header';
 import Button from './components/button/Button';
+import Icons from "./components/icons/Icons";
 
 function App() {
   return (
     <main className="container">
       <Header />
       <Button />
+      <Icons />
     </main>
   );
 }
--- src/components/icons/Icons.jsx
+++ src/components/icons/Icons.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import slack from '../../styles/assets/slack.png';
+import github from '../../styles/assets/github.png';
 import styles from '../icons/Icons.module.scss';
 
 const Icons = () => {
   return (
-    <div>
-      icons
+    <div className={styles.icons}>
+      <div>
+        <a href="https://hng9.slack.com/"><img src={slack} alt="Slack icon" /></a>
+
+        <a href="https://github.com/Sucodes"><img src={github} alt="GitHub icon" /></a>
+      </div>
     </div>
   )
 }
