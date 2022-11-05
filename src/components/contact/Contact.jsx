b86d93c46693509bc018f0b0da6c07bd814bb781
Oghenesuvwe Money <rhounamoney@gmail.com>
2022-11-05 20:34:16 +0100

    Contact page update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,43 +11,53 @@ const Contact = () => {
 
       <div className={styles.contact_form}>
         <form action="#">
-          <label htmlFor="First name">First name</label>
-          <input
-            type="text"
-            name="First name"
-            id="first_name"
-            placeholder="Enter your first name"
-          />
+          <div className={styles.name}>
+            <label htmlFor="First name">First name</label>
+            <input
+              type="text"
+              name="First name"
+              id="first_name"
+              placeholder="Enter your first name"
+            />
 
-          <label htmlFor="Last name">Last name</label>
-          <input
-            type="text"
-            name="Last name"
-            id="last_name"
-            placeholder="Enter your last name"
-          />
+            <label htmlFor="Last name">Last name</label>
+            <input
+              type="text"
+              name="Last name"
+              id="last_name"
+              placeholder="Enter your last name"
+            />
+          </div>
 
-          <label htmlFor="Email">Email</label>
-          <input
-            type="email"
-            name="Email"
-            id="email"
-            placeholder="yourname@email.com"
-          />
+          <div>
+            <label htmlFor="Email">Email</label>
+            <input
+              type="email"
+              name="Email"
+              id="email"
+              placeholder="yourname@email.com"
+            />
+          </div>
 
-          <label htmlFor="Message">Message</label>
-          <textarea
-            name="Message"
-            id="message"
-            cols="30"
-            rows="10"
-            placeholder="Send me a message and I'll reply you as soon as possible..."
-          ></textarea>
+          <div>
+            <label htmlFor="Message">Message</label>
+            <textarea
+              name="Message"
+              id="message"
+              cols="30"
+              rows="10"
+              placeholder="Send me a message and I'll reply you as soon as possible..."
+              required
+            ></textarea>
+          </div>
 
-          <input type="checkbox" name="Checkbox" id="checkbox" />
-          <label htmlFor="Checkbox">
-            You agree to providing your data to Suvwe Money who may contact you.
-          </label>
+          <div className={styles.checkbox}>
+            <input type="checkbox" name="Checkbox" id="checkbox"/>
+            <label htmlFor="Checkbox">
+              You agree to providing your data to Suvwe Money who may contact
+              you.
+            </label>
+          </div>
 
           <button id="btn_submit">Send message</button>
         </form>
